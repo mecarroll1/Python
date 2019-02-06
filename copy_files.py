5b7dc10576434294ce49e607921d31805ab962b3
mecarroll1 <46944384+mecarroll1@users.noreply.github.com>
2019-02-06 17:49:07 -0600

    Add files via upload

--- copy_files.py
+++ copy_files.py
@@ -6,25 +6,35 @@ One thing to note is that if reading in from a csv you get a list of lists
 If this happens you need to run the following loop to convert your list of
 lists to a list of strings if you do not do this the in statement will
 not work.'''
+
+import csv
+import shutil
+import os
+# file directory you want to copy from 
+Dir_copy = "C:\\Users\carroll1\Desktop\JPg_Images"
+# list address
+list_loc= "C:\\Users\carroll1\Desktop\Image_list2015.csv"
+# location you want files to be copied
+copy_loc = "C:\\Users\carroll1\Desktop\copied_images2015"
+# make directory you want to copy to
+os.mkdir(copy_loc)
+# read in list from a csv of file names
+with open(list_loc, 'rb') as f:
+	reader = csv.reader(f)
+	your_list = list(reader)
+
 ''' The following block of code will give you a list of strings from
-your list of lists:
+your list of lists:'''
 
 h = list()
 for i in your_list:
-	h.append(i[0])'''
+	h.append(i[0])
 
-''' To read in a CSV as a list from excel the following code is what I used:
 
-with open('image_list.csv', 'rb') as f:
-	reader = csv.reader(f)
-	your_list = list(reader)
-'''
 
 # os.walk goes through a directory file by file
 # shutil is a useful module for copying large amounts of data
-import os
-import shutil
-for root,directories, files in os.walk(os.getcwd()):
+for root,directories, files in os.walk(Dir_copy):
     for f in files:
         if f in h:
-            shutil.copy2(os.path.join(root, f),os.path.join("Copied_images", f))
+            shutil.copy2(os.path.join(root, f), copy_loc)
